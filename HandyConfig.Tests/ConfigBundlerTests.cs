414d23113d28490af472b059e5b3ea8908eb1312
Glory Lo <gloryl604@gmail.com>
2015-05-16 17:11:17 -0700

    added test stub w/ app config

--- HandyConfig.Tests/ConfigBundlerTests.cs
+++ HandyConfig.Tests/ConfigBundlerTests.cs
@@ -1,14 +1,29 @@
 ﻿using NUnit.Framework;
 using System;
+using HandyConfig.Configuration;
+using System.Collections.Generic;
 
 namespace HandyConfig.Tests
 {
     [TestFixture]
     public class ConfigBundlerTests
     {
-        [Test]
-        public void TestMethod1()
+        public NameValueTypeElementCollection Settings;
+        public ConfigurationBundler Bundler { get; set; }
+
+        [SetUp]
+        public void Setup()
         {
+            Settings = HandyConfigSection.Settings;
+            Bundler = new ConfigurationBundler(new Dictionary<string, object>(), Settings);
+        }
+
+        [Test]
+        public void LoadSettings()
+        {
+            
+
+
             Assert.Fail("test");
         }
     }
---
+++ HandyConfig/Configuration/HandyConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace HandyConfig.Configuration
+{
+    public class HandyConfigSection: ConfigurationSection
+    {
+        public static NameValueTypeElementCollection Settings =
+            ((HandyConfigSection) ConfigurationManager
+              .GetSection("handyconfig")).NameValueTypes;
+
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        [ConfigurationCollection(typeof(NameValueTypeElementCollection), AddItemName = "add")]
+        public NameValueTypeElementCollection NameValueTypes
+        {
+            get  { return (NameValueTypeElementCollection) this[""];  }
+            set  { this[""] = value;  }
+        }
+    }
+}
