a7363fed1ed3727279f6eb3aa07d0829de87c9c4
Glory Lo <gloryl604@gmail.com>
2015-05-16 17:54:33 -0700

    added unit tests for different types

--- HandyConfig.Tests/ConfigBundlerTests.cs
+++ HandyConfig.Tests/ConfigBundlerTests.cs
@@ -2,6 +2,7 @@
 using System;
 using HandyConfig.Configuration;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace HandyConfig.Tests
 {
@@ -19,12 +20,67 @@ namespace HandyConfig.Tests
         }
 
         [Test]
-        public void LoadSettings()
+        public void DuplicateLoadTest()
         {
-            
+            var configs = Bundler.Bundle();
+            Assert.IsTrue(configs.Count == 6);
+        }
 
+        private bool Compare<T>(object actual, T expected)
+        {
+            Trace.WriteLine("Expected: " + expected);
+            Trace.WriteLine("Actual: " + actual);
+            return actual.Equals(expected);
+        }
 
-            Assert.Fail("test");
+        [Test]
+        public void StringTypeTest()
+        {
+            var configs = Bundler.Bundle();
+            var check = Compare(configs["longstring"], "a long string");
+            Assert.IsTrue(check);
+        }
+
+        [Test]
+        public void IntValueTest()
+        {
+            var configs = Bundler.Bundle();
+            var check = Compare(configs["fiftythree"], 53);
+            Assert.IsTrue(check);
+        }
+
+        [Test]
+        public void TrueValueTest()
+        {
+            var configs = Bundler.Bundle();
+            var check = Compare(configs["booltrue"], true);
+            Assert.IsTrue(check);
+        }
+
+        [Test]
+        public void FalseValueTest()
+        {
+            var configs = Bundler.Bundle();
+            var check = Compare(configs["boolfalse"], false);
+            Assert.IsTrue(check);
+        }
+
+        [Test]
+        public void DateTest()
+        {
+            var configs = Bundler.Bundle();
+            var dec25 = new DateTime(1950, 12, 25);
+            var check = Compare(configs["date"], dec25);
+            Assert.IsTrue(check);
+        }
+
+        [Test]
+        public void DoubleTest()
+        {
+            var configs = Bundler.Bundle();
+            var aDouble = 100.99;
+            var check = Compare(configs["double"], aDouble);
+            Assert.IsTrue(check);
         }
     }
 }
