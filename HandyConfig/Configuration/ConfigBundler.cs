73a9dae04f54ff2008dbc40316f973efe17f8705
Glory Lo <gloryl604@gmail.com>
2015-05-17 18:20:26 -0700

    updated maintenance release

--- HandyConfig/Configuration/ConfigBundler.cs
+++ HandyConfig/Configuration/ConfigBundler.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HandyConfig.Configuration
-{
+{   
     public class ConfigBundler
     {
         private IDictionary<string, object> _configs;
