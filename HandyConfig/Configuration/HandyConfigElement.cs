3a5c7e4ad7283b78e776875fddebc96e185bf75a
Glory Lo <gloryl604@gmail.com>
2015-05-19 15:53:42 -0700

    added HandyConfigElement

---
+++ HandyConfig/Configuration/HandyConfigElement.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace HandyConfig.Configuration
+{
+    public class HandyConfigElement : ConfigurationElement
+    {
+        [ConfigurationProperty("", IsDefaultCollection = true)]
+        [ConfigurationCollection(typeof(NameValueTypeElementCollection), AddItemName = "add")]
+        public NameValueTypeElementCollection NameValueTypes
+        {
+            get { return (NameValueTypeElementCollection)this[""]; }
+            set { this[""] = value; }
+        }
+    } 
+    
+    
+}
